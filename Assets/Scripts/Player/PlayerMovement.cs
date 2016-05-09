0da3679ca5d6e5aa829cd0e2f1cfaf2715a6356e
elloserafim <elloserafim89@gmail.com>
2016-05-09 09:14:34 -0300

    Inicial

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floormask;
+    float camRayLenght = 100f;
+
+    void Awake()
+    {
+        floormask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    //Runs with physics
+    void FixedUptade()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime; // Deltatime is the time between each update
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorhit;
+        if (Physics.Raycast(camRay, out floorhit, camRayLenght, floormask))
+        {
+            Vector3 playertToMouse = floorhit.point - transform.position;
+            playertToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playertToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        
+    }
+}
